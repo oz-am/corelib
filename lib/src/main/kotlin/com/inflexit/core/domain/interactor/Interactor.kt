040e924abf7d987f4e176195c49edd82803827c8
Ossama Tahtah <ossama.tahtah@babelgroup.com>
2023-03-15 22:05:20 +0100

    first commit

---
+++ lib/src/main/kotlin/com/inflexit/core/domain/interactor/Interactor.kt
@@ -0,0 +1,41 @@
+package com.inflexit.core.domain.interactor
+
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletionStage
+
+interface Interactor<in Request, out Response> {
+    fun execute(request: Request): Response
+}
+
+interface InteractorExecutor {
+    operator fun <RequestDto, ResponseDto, Request, Response> invoke(
+        interactor: Interactor<Request, Response>,
+        requestDto: RequestDto,
+        requestConverter: (RequestDto) -> Request,
+        responseConverter: (Response) -> ResponseDto
+    ): CompletionStage<ResponseDto>
+
+    operator fun <RequestDto, Request> invoke(
+        interactor: Interactor<Request, Unit>,
+        requestDto: RequestDto,
+        requestConverter: (RequestDto) -> Request
+    ) = invoke(interactor, requestDto, requestConverter) {}
+
+    operator fun invoke(useCase: Interactor<Unit, Unit>) = invoke(useCase, Unit) {}
+    operator fun <ResponseDto, Response> invoke(
+        interactor: Interactor<Unit, Response>,
+        responseConverter: (Response) -> ResponseDto
+    ) = invoke(interactor, Unit, {}, responseConverter)
+}
+
+class UseCaseExecutorImpl : InteractorExecutor {
+    override fun <RequestDto, ResponseDto, Request, Response> invoke(
+        interactor: Interactor<Request, Response>,
+        requestDto: RequestDto,
+        requestConverter: (RequestDto) -> Request,
+        responseConverter: (Response) -> ResponseDto
+    ): CompletionStage<ResponseDto> = CompletableFuture
+        .supplyAsync { requestConverter(requestDto) }
+        .thenApplyAsync { interactor.execute(it) }
+        .thenApplyAsync { responseConverter(it) }
+}
